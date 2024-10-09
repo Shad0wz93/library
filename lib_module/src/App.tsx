81a278b0fded8fb361383c57ee02a9aa8ba49e8c
SukiTsu <buibrandon47@gmail.com>
2024-10-09 11:42:41 +0200

    Mise en place du composant Tabs. 4 pprops (pageCurrent, titles, links, overView) avec son style

--- lib_module/src/App.tsx
+++ lib_module/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
-import Modal from './components/organismes/modal/Modal.tsx'
+//import Modal from './components/organismes/modal/Modal.tsx'
+import Tabs from './components/organismes/Tabs'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
@@ -8,8 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
       {/*<Route path='/counter' element={<MyComponen/>} /> */}
-      <Route path='/' element={<Modal title='Mon titre' content="test" bkColor='red' position='left'/>} />
-        
+      {/*<Route path='/' element={<Modal title='Mon titre' content="test" bkColor='red' position='left'/>} /> */}
+      <Route path='/' element={<Tabs pageCurrent='Ici' titles={["page1","page2"]}  links={["#","#"]} overView="red"/>} /> 
 
       </Routes>
     </BrowserRouter>
---
+++ lib_module/src/components/organismes/Tabs.tsx
@@ -0,0 +1,30 @@
+import '../../assets/css/styleTabs.css'
+
+interface PropsTabs{
+    pageCurrent : string
+    titles : string[]
+    links : string[]
+    overView : string
+}
+
+const Tabs : React.FC<PropsTabs> = ({pageCurrent, titles, links, overView}) => {
+    const listTitlesLinks = []
+    for (let i=0;i<titles.length;i++){
+        listTitlesLinks.push(
+            <div className={`tab ${overView}`}>
+                <a href={links[i]}>{titles[i]}</a>
+            </div>
+        )
+    }
+    return(
+        <div className="tabs-container">
+            <div className="tabs-header">
+                <div className={`tab active ${overView}`}>{pageCurrent}</div>
+                {listTitlesLinks}
+            </div>
+        </div>
+    )
+
+}
+
+export default Tabs
