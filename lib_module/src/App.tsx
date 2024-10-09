8e2b2c3c9804341b26125727d53348de9803205b
SukiTsu <buibrandon47@gmail.com>
2024-10-09 11:58:59 +0200

    Mise en place du composant Tabs. 4 pprops (pageCurrent, titles, links, overView) avec son style

--- lib_module/src/App.tsx
+++ lib_module/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {Tabs} from './components/organismes/Tabs'
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path='/counter' element={<MonComposant/>} />
+      <Route path='/' element={<Tabs pageCurrent='Ici' titles={["page1","page2"]}  links={["#","#"]} overView="red"/>} /> 
         
 
       </Routes>
