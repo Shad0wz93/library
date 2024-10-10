371d50e64ab1d13022ef5a6f52a3f768c00670bb
SukiTsu <buibrandon47@gmail.com>
2024-10-10 11:20:30 +0200

    Ajout de la Doc Tabs

--- lib_module/src/components/organismes/Tabs.tsx
+++ lib_module/src/components/organismes/Tabs.tsx
@@ -1,12 +1,30 @@
 import '../../assets/css/styleTabs.css'
 
+/**
+ * Interface représentant la Tabs
+ */
 interface PropsTabs{
+    /** Nom de la page courrant */
     pageCurrent : string
+
+    /** Listes des titres présent dans la tabs qui auront des liens de redirection */
     titles : string[]
+
+    /** Liste des liens qui seront attribués aux titres */
     links : string[]
+
+    /** Couleur de fond du titre qui sera sur-volé */
     overView : string
 }
 
+/**
+ * Composant Tabs listant les liens sous forms de navbar
+ * @param pageCurrent - Nom de la page courrant où ce trouve le tabs
+ * @param titles - Listes des titres présent dans la tabs qui auront des liens de redirection
+ * @param links - Liste des liens qui seront attribués aux titres
+ * @param overView - Couleur de fond du titre qui sera sur-volé
+ * @returns Composant TSX Tabs
+ */
 const Tabs : React.FC<PropsTabs> = ({pageCurrent, titles, links, overView}) => {
     if (titles.length != links.length) return null
     const listTitlesLinks = []
