3485b9a326a7cdf463f81bc2c2822e7400ebddbe
SukiTsu <buibrandon47@gmail.com>
2024-10-10 14:20:15 +0200

    Ajout de la Card avec la Doc. Props: title, content, srcImg, bkColor.

--- lib_module/src/App.tsx
+++ lib_module/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Card from './components/molecules/card'
 
+const link = 'https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/09/Pokemon-Ash-Feature-Image-1.jpg?q=50&fit=crop&w=1140&h=&dpr=1.5'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path='/counter' element={<MonComposant/>} />
+      <Route path='/' element={<Card title='Mon titre' content="Ca c'est moi" srcImg={link} bkColor='green'/>} />
         
 
       </Routes>
