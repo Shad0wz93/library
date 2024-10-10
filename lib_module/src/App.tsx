7124c513250890f981e0bb0e11dbc0fa3125c6b8
bidibul75 <walter.bianchi@edu.esiee-it.fr>
2024-10-11 00:52:27 +0200

    Initial commit (tout fonctionne)

--- lib_module/src/App.tsx
+++ lib_module/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
+// Carousel
+import {images, interval, showcontrols} from './components/organismes/carousel/Carousel_data';
+import Carousel from './components/organismes/carousel/Carousel';
 // Alert
 import {type, message, autoClose, autoCloseTime} from './components/molecules/alert/Alert_data';
 import Alert from './components/molecules/alert/Alert';
@@ -19,6 +21,9 @@ function App() {
 
         {/* Route pour l'alerte */}
         <Route path='/alert' element={<Alert type={type} message={message} autoClose={autoClose} autoCloseTime={autoCloseTime} />} />
+        {/* Route pour le carousel */}
+
+        <Route path='/carousel' element={<Carousel images={images} interval={interval} showControls={showcontrols} />} />
 
       </Routes>
     </BrowserRouter>
