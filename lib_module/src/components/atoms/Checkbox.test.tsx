640cf99caf8ce5120efa020e2ae79c323624365d
SukiTsu <buibrandon47@gmail.com>
2024-10-11 10:15:17 +0200

    Validation des test

--- lib_module/src/components/atoms/Checkbox.test.tsx
+++ lib_module/src/components/atoms/Checkbox.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 import CheckBox from './Checkbox';
+import React from 'react';
 
 describe('CheckBox Component', () => {
   const mockProps = {
